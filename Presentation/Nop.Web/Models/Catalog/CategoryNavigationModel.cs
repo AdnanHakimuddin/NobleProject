f55d678a1b79ade7b5668a49573f4b281372ded5
Muhammad Uzair <muhammad.uzair@zaraye.co>
2023-01-27 19:11:15 +0500

    category navigation working done

--- Presentation/Nop.Web/Models/Catalog/CategoryNavigationModel.cs
+++ Presentation/Nop.Web/Models/Catalog/CategoryNavigationModel.cs
@@ -21,6 +21,8 @@ namespace Nop.Web.Models.Catalog
                 PartGroups = new List<CategorySimpleModel.PartGroupModel>();
             }
             public int CurrentCategoryId { get; set; }
+            public int CurrentPartGroupId { get; set; }
+            public bool ActivePartGroups { get; set; }
             public CategorySimpleModel Category { get; set; }
             public CategorySimpleModel.PartGroupModel PartGroup { get; set; }
             public List<CategorySimpleModel.PartGroupModel> PartGroups { get; set; }
